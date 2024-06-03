40d37f1058ab80836e85339fed9cc75c14c4e85a
3s7an <tristan.prekop13@gmail.com>
2024-06-03 19:37:41 +0200

    rezervacny formular

--- reservation.php
+++ reservation.php
@@ -9,7 +9,19 @@
     <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-    <h1>Book a massage here : </h1>
+    <h2>Reservation</h2>
+    <p>Let´s book a massage right now</p>
+    <p>You can call us at : 602229520</p>
+    <p>Write email on : tristan.prekop13@gmail.com</p>
+    <p>or simply use reservation form below</p>
+
+    <input type="text" placeholder="Name">
+    <input type="text" placeholder="Mobile number">
+    <input type="email" placeholder="Email">
+    <input type="textarea" placeholder="message">
+    
+    <p>We look forward to your visit, see you soon.</p>
+
 
     
     
